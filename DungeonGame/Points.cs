577cfbe64799e1a4f9a7301d7685505688768bbc
CarterRobbins1 <robbinsc3@my.lanecc.edu>
2025-06-13 22:28:09 -0700

    Created the simple points class to start keeping points for the run

--- DungeonGame/Points.cs
+++ DungeonGame/Points.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace DungeonGame
 {
-    internal class Points
+    public class Points
     {
+        private int score;
+
+        public Points()
+        {
+            score = 100;
+        }
+
+        public void Deduct(int amount=1)
+        {
+            score -= amount;
+            if (score < 0) score = 0; // Ensure score doesn't go negative
+        }
+
+        public int GetScore()
+        {
+            return score;
+        }
     }
 }
